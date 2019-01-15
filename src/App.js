4dd5873d787a5a3bf8331de487c82bd0b737e423
Vladimir Starkov <iamstarkov@gmail.com>
2019-01-15 11:47:37 +0100

    demo for jss#981

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import injectSheet, {ThemeProvider} from 'react-jss'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const theme = {};
+
+const A = injectSheet({ rootA: {} })(props => (<div className={props.classes.rootA} />));
+const B = injectSheet({ rootB: { color: p => !!p.whatever ? 'white' : 'black' } })(props => (<div className={props.classes.rootB} />));
+const C = injectSheet(theme => ({ rootC: {} }))(props => (<div className={props.classes.rootC} />));
+const D = injectSheet(theme => ({ rootD: { color: p => !!p.whatever ? 'white' : 'black' }}))(props => (<div className={props.classes.rootD} />));
+
+const AppRouter = () => (
+  <Router>
+    <ThemeProvider theme={theme}>
+      <div>
+        <nav>
+          <Link to="/a">Non-Themed, Non-Dynamic (WARNING)</Link>
+          <br />
+          <Link to="/b">Non-Themed, Dynamic (WARNING)</Link>
+          <br />
+          <Link to="/c">Themed, Non-Dynamic</Link>
+          <br />
+          <Link to="/d">Themed, Dynamic</Link>
+        </nav>
+        <Switch>
+          <Route path="/a" exact component={A} />
+          <Route path="/b" exact component={B} />
+          <Route path="/c" exact component={C} />
+          <Route path="/d" exact component={D} />
+          <Redirect from="/" to="/a" />
+        </Switch>
       </div>
-    );
-  }
-}
+    </ThemeProvider>
+  </Router>
+);
 
-export default App;
+export default AppRouter;
